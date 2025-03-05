3f1eddffe449ae2e18079fce21b473f90bc9d279
NayelieAvila <nayelie13avila@gmail.com>
2025-03-05 09:07:30 -0600

    Update PalinList.java

--- PalinList.java
+++ PalinList.java
@@ -1,12 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Nayelie
 
 import java.util.Queue;
 import java.util.Stack;
-import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class PalinList
@@ -21,17 +19,41 @@ public class PalinList
 
 	public PalinList(String list)
 	{
+		setList(list);
 	}
 
 	public void setList(String list)
 	{
+		queue = new LinkedList<>();
+		stack = new Stack<>();
+
+		String[] words = list.split(" ");
+		for (String word : words) {
+			queue.offer(word);
+			stack.push(word);
+		}
 	}
 
-	public boolean isPalin()
-	{
+	public boolean isPalin() {
+		while(!queue.isEmpty()) {
+			if(!queue.poll().equals(stack.pop())) {
+				return false;
+			}
+		}
 		return true;
 	}
 
 
 	//write a toString method
+
+	public String toString() {
+			return queue.toString() + (isPalin() ? " is a palinList. " : " is not a palinList.");
+
+		}
+	
 }
+
+
+
+
+
