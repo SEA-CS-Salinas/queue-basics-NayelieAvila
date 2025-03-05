4946d3ca00456911c5909d07dd9e299f15249338
NayelieAvila <nayelie13avila@gmail.com>
2025-03-05 08:44:47 -0600

    Update PalinListRunner.java

--- PalinListRunner.java
+++ PalinListRunner.java
@@ -1,12 +1,23 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Nayelie
 
-public class PalinListRunner
-{
-	public static void main ( String[] args )
-	{
-		//add test cases			
+public class PalinListRunner {
+	public static void main ( String[] args ) {
+		//add test cases	
+		String [] testCases {
+			"one two three two one",
+			"1 2 3 4 5 one two three four five",
+                        "a b c d e f g x y z g f h",
+                        "racecar is racecar",
+                        "1 2 3 a b c c b a 3 2 1",
+                        "chicken is a chicken"
+                   };
+
+		for(String testCase : testCase) {
+			PalinList palinList = new PalinList(testCase);
+			System.out.println(palinList);
+		}
 	}
 }
